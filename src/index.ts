b203df0eb18495a190763efc3c3cbf1f901feb21
tg云 <1518751112@qq.com>
2025-12-29 13:59:54 +0800

    fix:编译调整

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-if (process.env.NODE_ENV === 'production') {
-  require('module-alias/register')
-}
+import path, {join} from 'path'
 import express from 'express'
 import {middle} from "@lib/middle";
-import path from 'path'
 import {SERVER_PORT} from './config/env'
 import logger from '@lib/logger'
 import {startTasks} from '@lib/tasks'
 import {BackendDataScalerService} from "@/services/backend.datascaler";
 import fileRouter from "./routes/file";
 import {TaskService} from "@lib/tasks/task.server";
+
+//为了编译后能正确识别别名路径
+const moduleAlias = require('module-alias')
+moduleAlias.addAliases({
+  '@'  : __dirname,
+  '@lib': join(__dirname, 'lib'),
+})
 
 const app = express()
 //监听线程异常
