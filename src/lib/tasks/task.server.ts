d3ad9708a744aceb6bb3035562fd209cdfe24896
tg云 <1518751112@qq.com>
2025-12-30 11:38:40 +0800

    fix:改变字段结构

--- src/lib/tasks/task.server.ts
+++ src/lib/tasks/task.server.ts
@@ -25,7 +25,7 @@ function getChildTableFields() {
     return [
         {field_name: '日期', type: 'DateTime'},
         {field_name: '关联关键词', type: 'Text'},
-        {field_name: '流量占比', type: 'Text'},
+        {field_name: '流量占比', type: 'Number',property:{formatter: "0.00%"}},
         {field_name: '预估周曝光量', type: 'Number'},
         {field_name: '流量词类型', type: 'Text'},
         {field_name: '自然流量占比', type: 'Text'},
@@ -65,7 +65,7 @@ function mapKeywordToRecord(k: IKeywordData) {
     return {
         '关联关键词': k.keywords,
         '日期': nowTime,
-        '流量占比': toPercentage(k.trafficPercentage),
+        '流量占比': Number(((k.trafficPercentage||0)*100).toFixed(2)),
         '预估周曝光量': k.calculatedWeeklySearches ?? null,
         '流量词类型': k.position ?? null,
         '自然流量占比': toPercentage(k.naturalRatio ?? null),
